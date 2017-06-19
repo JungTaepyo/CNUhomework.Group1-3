dc5a0b297a380a1271199092b0b36f701c95ffa1
Jaesung Chris NA <na-0003@hotmail.com>
2017-06-19 22:21:53 +0900

    dealCard() - test : return class is Card

--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,7 +1,10 @@
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.util.Assert.isInstanceOf;
+
 /**
  * Created by ryan on 2017. 6. 19..
  */
@@ -11,6 +14,12 @@ public class DeckTest {
     public void Check_Cardsused(){
         Deck deck = new Deck();
         assertThat(deck.cardsLeft(),is(52));
+    }
+    @Test
+    public void DEAL_CARD_CHECK_WHETHER_IT_RETURNS_A_CARD_FROM_DECK(){
+        Deck deck = new Deck();
+        Card returnCard = deck.dealCard();
+        assertThat(returnCard, instanceOf(Card.class));
     }
 
 }
