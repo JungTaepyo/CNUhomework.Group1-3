89daeca1358c908328e97e8c2f496f65d44c748b
ChoiKwangHo <dk00426@naver.com>
2017-06-19 17:10:36 +0900

    class card 카드 변수생성

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -4,4 +4,9 @@
 import java.io.Serializable;
 public class Card implements Serializable {
 
+    public final static int SPADES = 0;
+    public final static int HEARTS = 1;
+    public final static int DIAMONDS = 2;
+    public final static int CLUBS = 3;
+
 }
