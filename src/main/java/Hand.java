932179708068156ad9e9aed3497e949c9b07dae4
LeeChungWan <cnddhks846@naver.com>
2017-06-19 21:02:09 +0900

    sortByValue 값 비교하기

--- src/main/java/Hand.java
+++ src/main/java/Hand.java
@@ -45,6 +45,13 @@ public class Hand {
         while(hand.size() > 0){
             int pos = 0;
             Card c = (Card)hand.elementAt(0);
+            for(int i = 1; i < hand.size(); i++){
+                Card c1 = (Card)hand.elementAt(i);
+                if(c1.getValue() < c.getValue()){
+                    pos = i;
+                    c = c1;
+                }
+            }
         }
     }
 
