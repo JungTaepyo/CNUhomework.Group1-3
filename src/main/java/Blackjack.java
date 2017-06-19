2b8c08c22214854d14f64129cd3871c328ca52ab
koseokkyu <dighrh@naver.com>
2017-06-19 21:53:38 +0900

    pull이전 commit

--- src/main/java/Blackjack.java
+++ src/main/java/Blackjack.java
@@ -25,14 +25,30 @@ public class Blackjack implements Serializable {
         while (true) {
             System.out.println("You have " + money + " dollars.");
             do {
+
+                // 예외처리: 사용자입력이 정수가 아닌 다른 type을 받는 경우
+
+                while (true) {
                     System.out.println("How many dollars do you want to bet?  (Enter 0 to end.)");
                     System.out.print("? ");
                     scannedToken = scan.next();// 사용자에게 문자열을 입력받음
+                    try {
+                        // 정수인 경우: 입력받은 문자열을 정수로 변환하여 bet에 저장하고 break
+                        bet = Integer.parseInt(scannedToken);
+                        break;
+                    } catch (NumberFormatException e) {
+                        // 입력받은 문자열이 정수가 아닌 경우
+                        System.out.println("! Error: 입력이 잘못 되었습니다  (" + scannedToken + ")");
+                    } finally {
+                        System.out.println("! User input type has been checked");
+                    }
+                }
 
-                    bet = Integer.parseInt(scannedToken);
                 if (bet < 0 || bet > money)
                     System.out.println("Your answer must be between 0 and " + money + '.');
             } while (bet < 0 || bet > money);
+            if (bet == 0)
+                break;
         }
     }
 
