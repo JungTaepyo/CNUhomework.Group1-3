491fae15d0feee83b1dcc64c8c2d78d48599411c
Joh <jms5049@naver.com>
2017-06-19 20:26:46 +0900

    first test method, CardTest

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,5 +1,14 @@
+import org.junit.Test;
+
+import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Created by user on 2017-06-19.
  */
 public class CardTest {
+
+    @Test
+    public void 카드가_잘_작동하나(){
+    }
 }
