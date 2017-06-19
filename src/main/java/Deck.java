e4831394a9a25aa0751fb4a0e1a793ef38c0499e
kimjinhyeok <persona5173@gmail.com>
2017-06-19 16:55:57 +0900

    modified Deck_add implements Serializable

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,7 +1,9 @@
+import java.io.Serializable;
+
 /**
  * Created by user on 2017-06-19.
  */
-public class Deck {
+public class Deck implements Serializable {
     private Card[] deck;
     private int cardsUsed;
 }
