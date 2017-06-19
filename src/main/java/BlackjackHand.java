21a7666dda8a8f21afcad37c8bba449a11e28117
kimjinhyeok <persona5173@gmail.com>
2017-06-19 16:53:00 +0900

    make blackjackHand class

---
+++ src/main/java/BlackjackHand.java
@@ -0,0 +1,13 @@
+/**
+ * Created by user on 2017-06-19.
+ */
+public class BlackjackHand {
+    public int getBlackjackValue() { //----------Edited----------//
+        //특정 Hand에 있는 블랙잭 합 계산
+
+        int value = 21; //최댓값
+
+        return value;
+
+    }
+}
