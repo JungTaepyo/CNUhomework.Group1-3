f89705b10b145d2b57eb3f9b8d7a798408316250
kimjinhyeok <persona5173@gmail.com>
2017-06-19 17:41:56 +0900

    블랙잭_핸드_개수 method test 구현

--- src/test/java/BlackjackHandTest.java
+++ src/test/java/BlackjackHandTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by user on 2017-06-19.
@@ -9,6 +11,7 @@ public class BlackjackHandTest {
     @Test
     public void 블랙잭_핸드_개수_테스트(){
         BlackjackHand blackjackHand = new BlackjackHand();
+        assertThat(blackjackHand.getBlackjackValue(), is(21));
     }
 
 }
