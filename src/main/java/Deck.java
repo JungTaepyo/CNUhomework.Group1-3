bf30d744a5aa9b59e933b5e9644dffefbacbbbf6
Kihyun Choi <ryan@Kihyunui-MacBook-Pro.local>
2017-06-19 20:22:17 +0900

    Created variable for constructor

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -8,8 +8,11 @@ public class Deck implements Serializable {
     private int cardsUsed;
 
     public Deck() {
-        //섞이지 않은 카드 덱 생
+        //섞이지 않은 카드 덱 생성
         deck = new Card[52];
+        int cardCount = 0; // 카드를 추가할때마다 증가
+
+
     }
 
     public void shuffle() { //사용한 카드를 다시 덱에 넣어서 shuffle
