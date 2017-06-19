ad4e5c5d724a472f917505d61075988c1ec0c79d
Joh <jms5049@naver.com>
2017-06-19 20:27:59 +0900

    CardTest test method, success

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -10,5 +10,8 @@ public class CardTest {
 
     @Test
     public void 카드가_잘_작동하나(){
+        Card firstCard = new Card(1,1);
+        Card secondCard = new Card(1,1);
+        assertThat(firstCard==secondCard);
     }
 }
