366dcc7ed52775eb7dd24e22cdaf547d18e5b723
ChoiKwangHo <dk00426@naver.com>
2017-06-19 17:11:44 +0900

    class card 게임 변수 생성

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -8,5 +8,8 @@ public class Card implements Serializable {
     public final static int HEARTS = 1;
     public final static int DIAMONDS = 2;
     public final static int CLUBS = 3;
-
+    public final static int ACE = 1 ;
+    public final static int JACK = 11 ;
+    public final static int QUEEN = 12 ;
+    public final static int KING = 13 ;
 }
